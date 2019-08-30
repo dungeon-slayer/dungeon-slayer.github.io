3474878a1617f4c1f8ce867abc75fcae492262d7
dungeon-slayer <53002629+dungeon-slayer@users.noreply.github.com>
2019-08-30 21:01:19 +1000

    Combine  character and ability sections, into 'character' section

--- src/components/SectionCharacter.tsx
+++ src/components/SectionCharacter.tsx
@@ -4,15 +4,15 @@ import { Dispatch } from 'redux'
 import styled from 'styled-components'
 import * as Bows from 'bows'
 import { StoreState } from 'src/store/interface'
-import { ConsumableItem, DropItem } from 'src/data'
+import { ConsumableItem, DropItem, AbilityItem } from 'src/data'
 import { PlayerState, BattleState } from 'src/reducers'
-import { PlayerHelper } from 'src/helpers'
+import { PlayerHelper, AbilityHelper } from 'src/helpers'
 import { PlayerAction } from 'src/actions'
 import ListItem from './ListItem'
 import { mediaQueries } from 'src/constants'
-import { CtaItem } from 'src/common/interfaces'
+import { CtaItem, ActiveAbilityItem } from 'src/common/interfaces'
 
-const log = Bows('SectionInventory')
+const log = Bows('SectionCharacter')
 
 const ComponentWrapper = styled.div``
 
@@ -39,6 +39,13 @@ const DescriptionWrapper = styled.div`
   margin: 4px 0;
 `
 
+const EmphasisDescriptionWrapper = styled.div`
+  margin-top: 12px;
+  background-color: rgba(255, 184, 98, 0.1);
+  border-radius: 4px;
+  padding: 12px;
+`
+
 const NoContentWrapper = styled(DescriptionWrapper)`
   color: #dc0073;
 `
@@ -55,9 +62,10 @@ interface Props {
   player: PlayerState
   battle: BattleState
   useConsumable: (consumable: ConsumableItem) => Promise<void>
+  toggleAbility: (ability: AbilityItem) => Promise<void>
 }
 
-class BaseSectionInventory extends React.Component<Props> {
+class BaseSectionCharacter extends React.Component<Props> {
   componentDidMount() {
     log('componentDidMount triggered.')
   }
@@ -66,15 +74,21 @@ class BaseSectionInventory extends React.Component<Props> {
     log('componentWillUnmount triggered.')
   }
 
-  async operatorClickHandler(consumable: ConsumableItem) {
-    log('operatorClickHandler triggered. consumable:', consumable)
+  async consumableClickHandler(consumable: ConsumableItem) {
+    log('consumableClickHandler triggered. consumable:', consumable)
     await this.props.useConsumable(consumable)
+  }
+
+  async abilityClickHandler(ability: AbilityItem) {
+    log('abilityClickHandler triggered. ability:', ability)
+    await this.props.toggleAbility(ability)
   }
 
   render(): JSX.Element {
     return (
       <ComponentWrapper>
         {this.renderCaption()}
+        {this.renderAbilities()}
         {this.renderConsumables()}
         {this.renderDrops()}
       </ComponentWrapper>
@@ -84,9 +98,63 @@ class BaseSectionInventory extends React.Component<Props> {
   private renderCaption(): JSX.Element {
     return (
       <CaptionContainer role="heading" aria-level={1}>
-        Inventory
+        Character
       </CaptionContainer>
     )
+  }
+
+  private renderAbilities(): JSX.Element {
+    const availableAbilities = PlayerHelper.getAvailableAbilityItems(this.props.player)
+    const usedAP = PlayerHelper.getUsedAbilityPoint(this.props.player)
+    const totalAP = PlayerHelper.getTotalAbilityPoint(this.props.player.character!.currentLevel)
+
+    let dynamicContent: JSX.Element
+    if (availableAbilities.length === 0) {
+      dynamicContent = <NoContentWrapper>You do not have any abilities available.</NoContentWrapper>
+    } else {
+      dynamicContent = <React.Fragment>{availableAbilities.map((item) => this.renderAbility(item))}</React.Fragment>
+    }
+
+    return (
+      <React.Fragment>
+        <SubcaptionContainer role="heading" aria-level={2}>
+          Abilities
+        </SubcaptionContainer>
+        <DescriptionWrapper>Abilities you have acquired throughout the adventure.</DescriptionWrapper>
+        <EmphasisDescriptionWrapper>
+          You allocated <strong>{usedAP}</strong> out of total <strong>{totalAP}</strong> Ability Points.
+        </EmphasisDescriptionWrapper>
+        <ConsumableContainer>{dynamicContent}</ConsumableContainer>
+      </React.Fragment>
+    )
+  }
+
+  private renderAbility(aaItem: ActiveAbilityItem): JSX.Element | null {
+    const ability = AbilityHelper.getItemByKey(aaItem.key)
+
+    if (!ability) {
+      log('Failed to find ability:', aaItem)
+      return null
+    }
+
+    const availableAP = PlayerHelper.getAvailableAbilityPoint(this.props.player)
+    const apCost = AbilityHelper.getApCost(ability, aaItem.level)
+
+    const heading = AbilityHelper.getFullName(ability, aaItem.level)
+    const subheading = `(AP: ${apCost})`
+    const flavor = AbilityHelper.getFlavorText(ability, aaItem.level)
+
+    const ctaItem: CtaItem = {
+      type: availableAP > apCost ? 'red' : 'disabled',
+      label: 'Off',
+      onClick: () => this.abilityClickHandler(ability),
+    }
+    if (AbilityHelper.isActivated(this.props.player, ability.key)) {
+      ctaItem.type = 'green'
+      ctaItem.label = 'On'
+    }
+
+    return <ListItem key={ability.key} heading={heading} subheading={subheading} flavor={flavor} ctaItems={[ctaItem]} ctaMinWidth="100px" />
   }
 
   private renderConsumables(): JSX.Element {
@@ -125,7 +193,7 @@ class BaseSectionInventory extends React.Component<Props> {
     const ctaItem: CtaItem = {
       type: 'blue',
       label: 'Use',
-      onClick: () => this.operatorClickHandler(consumable),
+      onClick: () => this.consumableClickHandler(consumable),
     }
     if (PlayerHelper.isInFightingMode(this.props.player, this.props.battle)) {
       ctaItem.type = 'disabled'
@@ -185,11 +253,15 @@ function mapDispatchToProps(dispatch: Dispatch) {
     useConsumable: async (consumable: ConsumableItem): Promise<void> => {
       await dispatch(PlayerAction.useConsumable(consumable))
     },
+
+    toggleAbility: async (ability: AbilityItem): Promise<void> => {
+      await dispatch(PlayerAction.toggleAbility(ability))
+    },
   }
 }
 
-const SectionInventory = connect(
+const SectionCharacter = connect(
   mapStateToProps,
   mapDispatchToProps
-)(BaseSectionInventory)
-export default SectionInventory
+)(BaseSectionCharacter)
+export default SectionCharacter
--- src/data/sections.ts
+++ src/data/sections.ts
@@ -1,11 +1,9 @@
 import MyLocationIcon from '@material-ui/icons/MyLocation'
 import ImportContactsIcon from '@material-ui/icons/ImportContacts'
-import SurroundSoundIcon from '@material-ui/icons/SurroundSound'
 import AllInboxIcon from '@material-ui/icons/AllInbox'
 import SettingsIcon from '@material-ui/icons/Settings'
 import SectionMap from 'src/components/SectionMap'
-import SectionAbility from 'src/components/SectionAbility'
-import SectionInventory from 'src/components/SectionInventory'
+import SectionCharacter from 'src/components/SectionCharacter'
 import SectionLocation from 'src/components/SectionLocation'
 import { IconComponent } from 'src/common/interfaces'
 import SectionSettings from 'src/components/SectionSettings'
@@ -31,15 +29,9 @@ export const sections: SectionItem[] = [
     Icon: ImportContactsIcon,
   },
   {
-    key: 'ability',
-    name: 'Ability',
-    Component: SectionAbility,
-    Icon: SurroundSoundIcon,
-  },
-  {
-    key: 'inventory',
-    name: 'Inventory',
-    Component: SectionInventory,
+    key: 'character',
+    name: 'Character',
+    Component: SectionCharacter,
     Icon: AllInboxIcon,
   },
   {
